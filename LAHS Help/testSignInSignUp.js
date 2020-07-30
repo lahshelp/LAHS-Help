9d9d186f9530f24c7e75ab3307cc1ab461575f9d
lahshelp <68867784+lahshelp@users.noreply.github.com>
2020-07-30 11:55:22 -0700

    Update testSignInSignUp.js

--- LAHS Help/testSignInSignUp.js
+++ LAHS Help/testSignInSignUp.js
@@ -1,7 +1,8 @@
-function signIn(){
-  alert("Sign In test function call")
-}
 
-function signUp(){
-  alert("Sign Up test function call")
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
